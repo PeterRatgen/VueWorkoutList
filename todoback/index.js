e55089d0652d56b67d2b247821a7e49d93776a3a
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-03-07 19:45:57 +0100

    test

--- todoback/index.js
+++ todoback/index.js
@@ -1,7 +1,10 @@
-let http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
+var server = app.listen(3001, function () {
+  console.log("Listening on port 3001")
+})
