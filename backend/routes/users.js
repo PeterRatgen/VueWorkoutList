d06933783b069fb5fb74884553563f27b763fd1d
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-30 00:01:16 +0200

    readd get request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -24,10 +24,10 @@ function authenticateToken(req, res, next) {
 }
 
 router.post("/user", user_controller.user_post);
-//router.get("/user/:userId", user_controller.user_get);
+router.get("/user/:userId", user_controller.user_get);
 router.delete("/user/:userId", user_controller.user_delete);
 
-router.post("/user/generateToken", user_controller.generate_token)
-router.get("/user/validateToken", authenticateToken, user_controller.validate_token)
+router.post("/user/generateToken", user_controller.generate_token);
+router.get("/user/validateToken", authenticateToken, user_controller.validate_token);
 
 module.exports = router;
