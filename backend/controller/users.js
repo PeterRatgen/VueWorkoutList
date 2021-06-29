7e1a619a781286a71571d558a13899e397e267b4
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-29 22:05:18 +0200

    Token

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -1,6 +1,7 @@
 let mongo = require('mongodb');
 const {ObjectId} = require('mongodb');
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 dotenv.config();
 
 exports.user_post = function(req, res) {
@@ -51,4 +52,40 @@ exports.user_delete = function(req, res) {
 			db.close();
 		});
 	});
+}
+
+exports.generate_token = function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    let data = {
+        time: Date(),
+        userId: 12,
+    }
+  
+    const token = jwt.sign(data, jwtSecretKey);
+    res.send(token);
+}
+
+
+exports.validate_token = function(req, res, err) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+  
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            return res.send("Successfully Verified");
+        } else{
+            // Access Denied
+            return res.status(401).send(err);
+        }
+    } catch (err) {
+        // Access Denied
+        return res.status(401).send(err);
+    }
+
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-user_controller = require('../controller/users.js')
+const user_controller = require('../controller/users.js')
 
 router.post("/user", user_controller.user_post);
 router.get("/user/:userId", user_controller.user_get);
 router.delete("/user/:userId", user_controller.user_delete);
+
+router.post("/user/generateToken", user_controller.generate_token)
+router.get("/user/validateToken", user_controller.validate_token)
 
 module.exports = router;
