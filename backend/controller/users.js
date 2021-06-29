16566146dbfe1e435f2c809ad6ac49a02dfd4f99
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-30 00:24:57 +0200

    add use

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -65,5 +65,5 @@ exports.generate_token = function(req, res) {
 
 exports.validate_token = function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send("authenticated " + req.userId);
+    res.send("authenticated " + req["userId"]);
 }
