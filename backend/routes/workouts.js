40e096ca27068c719cd6c9e492c32706f234feac
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-29 11:27:29 +0200

    Use router instead of app

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 workout_controller = require('../controller/workout.js')
 
-app.post("/workout/:userId", workout_controller.workout_post);
-app.get("/workout/:userId", workout_controller.workout_get);
-app.delete("/workout/:workoutId", workout_controller.workout_delete);
+router.post("/workout/:userId", workout_controller.workout_post);
+router.get("/workout/:userId", workout_controller.workout_get);
+router.delete("/workout/:workoutId", workout_controller.workout_delete);
 
 module.exports = router
