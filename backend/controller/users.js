8d6916645291bd78357a5a2ffc8da18f720c51fa
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-30 12:29:50 +0200

    Use user instead of userId

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -117,5 +117,5 @@ exports.login = async function(req, res) {
 
 exports.validate_token = function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send("authenticated " + JSON.stringify(req.userId));
+    res.send("authenticated " + JSON.stringify(req.user));
 }
