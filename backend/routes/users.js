69e4c3531f25653b88c602f8dd3d8b1fb3cb8b1d
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-07-04 12:52:41 +0200

    Add allow_all to routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,11 +7,11 @@ const cors = require('../middleware/cors_allow_all.js')
 const user_controller = require('../controller/users.js')
 
 
-router.post('/user', user_controller.user_post);
-router.get('/user/:userId', authentication.authenticate_token, user_controller.user_get);
-router.delete('/user/:userId', user_controller.user_delete);
+router.post('/user', cors.allow_all, user_controller.user_post);
+router.get('/user/:userId', cors.allow_all, authentication.authenticate_token, user_controller.user_get);
+router.delete('/user/:userId', cors.allow_all, user_controller.user_delete);
 
-router.post('/login', user_controller.login);
+router.post('/login', cors.allow_all, user_controller.login);
 router.get('/user_validateToken', authentication.authenticate_token , user_controller.validate_token);
 
 module.exports = router;
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 
 const authentication = require('../middleware/authentication.js')
 const workout_controller = require('../controller/workout.js')
+const cors = require('../middleware/cors_allow_all.js')
 
-router.post("/workout", authentication.authenticate_token, workout_controller.workout_post);
-router.get("/workout", authentication.authenticate_token, workout_controller.workout_get);
-router.delete("/workout/:workoutId", authentication.authenticate_token, workout_controller.workout_delete);
+router.post("/workout", cors.allow_all,  authentication.authenticate_token, workout_controller.workout_post);
+router.get("/workout", cors.allow_all, authentication.authenticate_token, workout_controller.workout_get);
+router.delete("/workout/:workoutId", cors.allow_all, authentication.authenticate_token, workout_controller.workout_delete);
 
 module.exports = router
