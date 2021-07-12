e1a2ecffe0b37eb53ee7e585106d098e2dfb9e82
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-07-13 00:33:24 +0200

    indentifier

--- backend/controller/workout.js
+++ backend/controller/workout.js
@@ -107,7 +107,7 @@ exports.workout_put_exercise_name = function(req, res) {
             }
         }
         let arrayFilters = [
-            { arrayFilters : [ {"element" : { $index : body.exerciseIndex } } ] }     
+            { arrayFilters : [ {element : { $position : body.exerciseIndex } } ] }     
         ]
 		dbase.collection("workouts").updateOne(
             query, 
