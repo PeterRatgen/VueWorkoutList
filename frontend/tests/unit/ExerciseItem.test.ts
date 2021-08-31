bd29ca9ae4b0a3502b3213b702cc11a274315223
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-08-31 19:55:57 +0200

    Testing on ExerciseItem
        - Missing testing on the expanded ExerciseItem
    
    references issue #8

--- frontend/tests/unit/ExerciseItem.test.ts
+++ frontend/tests/unit/ExerciseItem.test.ts
@@ -5,13 +5,12 @@ import ExerciseItem from '../../src/components/ExerciseItem.vue';
 import Repetition from '../../src/components/Repetition.vue';
 
 import { exercise } from '../testData';
-
 import mitt from 'mitt';
 
 const wrapper = shallowMount(ExerciseItem, {
     data() {
         return {
-            contracted : false
+            contracted : true
         };
     },
     props : {
@@ -25,8 +24,45 @@ const wrapper = shallowMount(ExerciseItem, {
     }
 });
 
-describe('Test for existence of components', () => {
-    test('Repetition component', () => {
+describe('The closed exerciseItem', () => {
+    const header =  wrapper.find('[class="clickable-header"]');
+    it('has a header with contents', () => {
+        expect(header.exists()).toBeTruthy();
+        expect(header.text()).toBe(exercise.name);
+    });  
+
+    it('does not display when not hovering', () => {
+        const iconContainer =  wrapper.find('[class="icon-container"]');
+        expect(iconContainer.exists()).toBeFalsy();
+    });
+
+    it('displays trashcan when hovering', async () => {
+        const nameHover =  wrapper.find('[class="name"]');
+        await nameHover.trigger('mouseover');
+        const iconContainer =  wrapper.find('[class="icon-container"]');
+        expect(iconContainer.exists()).toBeTruthy();
+    });
+
+    it('emits delete-exercise on click of trashcan', async() => {
+        const iconContainer =  wrapper.find('[class="icon-container"]');
+        await iconContainer.trigger('click');
+
+        const deleteEvent = wrapper.emitted('delete-exercise');
+        expect(deleteEvent).toHaveLength(1);
+    });
+
+
+    it('has no Repetition component', () => {
+        expect(wrapper.findComponent(Repetition).exists()).toBeFalsy();
+    });
+});
+
+describe('Contain components', () => {
+
+    test('repetition component', async () => {
+        const nameHover =  wrapper.find('[class="name"]');
+        await nameHover.trigger('click');
+
         expect(wrapper.getComponent(Repetition)).toBeTruthy();
     });
 });
