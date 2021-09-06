bf7b53abd85f793f0f0935a19504832c9af64bab
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-09-06 19:47:33 +0200

    Add testing to the WorkoutFormAdder component
    closes #14

--- frontend/tests/unit/WorkoutFormAdder.test.ts
+++ frontend/tests/unit/WorkoutFormAdder.test.ts
@@ -2,10 +2,16 @@ import 'jest';
 import { shallowMount } from '@vue/test-utils';
 
 import WorkoutFormAdder from '../../src/components/WorkoutFormAdder.vue';
+import NewExercise from '../../src/components/NewExercise.vue';
+import ExerciseItem from '../../src/components/ExerciseItem.vue';
 
-import { testData } from '../testData';
+import { workout } from '../testData';
 
-const wrapper = shallowMount(WorkoutFormAdder);
+const wrapper = shallowMount(WorkoutFormAdder, {
+    props : {
+        exerciseList : workout.exerciseList
+    }
+});
 
 describe("The WorkoutFormAdder component", () => {
     it('can set input value', async() =>{
@@ -13,5 +19,21 @@ describe("The WorkoutFormAdder component", () => {
         await input.setValue("Exercise title");
 
         expect((input.element as any).value).toBe("Exercise title");
+    });
+    
+    it('can trigger input, and emits event', async() => {
+        let input = wrapper.find('[class="header-input"]');
+        await input.trigger("input");
+
+        expect(wrapper.emitted()).toHaveProperty("workout-header");
+    });
+
+    it('displays all the exercises in prop', async() => {
+        let exercises = wrapper.findAllComponents(ExerciseItem);
+        expect(exercises.length).toBe(workout.exerciseList.length);
+    });
+
+    it('contains the NewExercise component', async() => {
+        expect(wrapper.findComponent(NewExercise).exists()).toBeTruthy();
     });
 });
