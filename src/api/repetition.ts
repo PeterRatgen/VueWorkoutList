c68596afa681e813080827364d8efb0fd0ce2edc
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-09-30 10:03:40 +0200

    Add function return types and equality

--- src/api/repetition.ts
+++ src/api/repetition.ts
@@ -1,10 +1,10 @@
 import { AxiosInstance } from 'axios'
 
-import { IWorkout, repData } from '../types/index'
+import { repData } from '../types/index'
 
 export async function addRepetition (apiInstance : AxiosInstance, data : repData) : Promise<{repetitionId : string} | undefined> {
   apiInstance.put('/workout/add_repetition', data).then((res) => {
-    if (res.status == 200) {
+    if (res.status === 200) {
       return { repetitionId: res.data }
     } else {
       return undefined
@@ -19,27 +19,26 @@ export async function deleteRepetition (apiInstance : AxiosInstance, data : {
     workoutId : string,
     exerciseId : string,
     repetitionId : string
-}) {
+}) : Promise<void> {
   apiInstance.put('/workout/delete_rep', data).then((res) => {
-    if (res.status == 200) {
+    if (res.status === 200) {
 
     } else {
-      new Error('API call to delete rep failed')
+      Error('API call to delete rep failed')
     }
   })
 }
 
-export async function changeRepetition (
-  apiInstance : AxiosInstance, data : repData) {
+export async function changeRepetition (apiInstance : AxiosInstance, data : repData) : Promise<void> {
   apiInstance.put('/workout/rep_change', {
     workoutId: data.workoutId,
     exerciseId: data.exerciseId,
     repItem: data.repItem
   }).then((res) => {
-    if (res.status == 200) {
+    if (res.status === 200) {
       console.log('changed rep to ' + JSON.stringify(data.repItem))
     } else {
-      new Error('API call to change rep failed')
+      Error('API call to change rep failed')
     }
   }).catch((err) => {
     console.trace()
