0e02596065dfc612df3129c2c85bbdbe61311b45
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-09-30 10:52:48 +0200

    Add userdata type

--- src/store/state_type.ts
+++ src/store/state_type.ts
@@ -1,12 +1,16 @@
 import { IWorkout } from '../types/index'
 import { AxiosInstance } from 'axios'
 
+export interface UserData{
+  name : string
+}
+
 export interface State {
     workouts : IWorkout[] | undefined,
     workingOut : boolean,
     currentWorkout: IWorkout | undefined,
     apiInstance : AxiosInstance,
-    userData : Object,
+    userData : UserData,
     loading: boolean,
     email : string,
     password : string
