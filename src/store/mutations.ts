b235a73ee8c3704fcef8f0527ad209f849bb374b
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-09-30 09:50:25 +0200

    Fix linting
    Fix equality, and function return types. Missing removal of explicit
    any types.

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -15,38 +15,38 @@ export const mutations = {
   setUserData (state : State, userData : Object) : void {
     state.userData = userData
   },
-  addRepetition (state : State, data : repData) {
-    if (state.workouts != undefined) {
+  addRepetition (state : State, data : repData) : void {
+    if (state.workouts !== undefined) {
       const workout: IWorkout | undefined =
-                state.workouts.find(element => element._id == data.workoutId)
-      if (workout != undefined) {
+                state.workouts.find(element => element._id === data.workoutId)
+      if (workout !== undefined) {
         const exercise: IExercise | undefined =
                     workout.exerciseList.find(
-                      (element: any) => element.id == data.exerciseId
+                      (element: any) => element.id === data.exerciseId
                     )
-        if (exercise != undefined) {
+        if (exercise !== undefined) {
           exercise.set.push(data.repItem)
         }
       }
     }
   },
-  changeRepetition (state : State, data : repData) {
-    if (state.workouts != undefined) {
+  changeRepetition (state : State, data : repData) : void {
+    if (state.workouts !== undefined) {
       const workout : IWorkout | undefined =
                 state.workouts.find(
-                  (element : IWorkout) => element._id == data.workoutId
+                  (element : IWorkout) => element._id === data.workoutId
                 )
-      if (workout != undefined) {
+      if (workout !== undefined) {
         const exercise : IExercise | undefined =
                     workout.exerciseList.find(
-                      (element : IExercise) => element.id == data.exerciseId
+                      (element : IExercise) => element.id === data.exerciseId
                     )
-        if (exercise != undefined) {
+        if (exercise !== undefined) {
           let rep : IRepetition | undefined =
                         exercise.set.find(
-                          (element : IRepetition) => element.id == data.repItem.id
+                          (element : IRepetition) => element.id === data.repItem.id
                         )
-          if (rep != undefined) {
+          if (rep !== undefined) {
             rep = data.repItem
           }
           return undefined
@@ -58,20 +58,20 @@ export const mutations = {
   },
   deleteWorkout (state : State, data : {
         workoutId : string
-    }) {
-    if (state.workouts != undefined) {
+    }) : void {
+    if (state.workouts !== undefined) {
       const ele: IWorkout | undefined =
-                state.workouts.find(element => element._id == data.workoutId)
-      if (ele != undefined) {
+                state.workouts.find(element => element._id === data.workoutId)
+      if (ele !== undefined) {
         const index: number = state.workouts.indexOf(ele)
-        if (index != -1) {
+        if (index !== -1) {
           state.workouts.splice(index, 1)
         }
       }
     }
   },
-  addWorkout (state : State, data : IWorkout) {
-    if (state.workouts != undefined) {
+  addWorkout (state : State, data : IWorkout) : void {
+    if (state.workouts !== undefined) {
       state.workouts.push(data)
     }
   },
@@ -79,18 +79,18 @@ export const mutations = {
         workoutId : string,
         exerciseId : string,
         name : string
-    }) {
-    if (state.workouts != undefined) {
+    }) : void {
+    if (state.workouts !== undefined) {
       const workout : IWorkout | undefined =
                 state.workouts.find(
-                  (element : IWorkout) => element._id == data.workoutId
+                  (element : IWorkout) => element._id === data.workoutId
                 )
-      if (workout != undefined) {
+      if (workout !== undefined) {
         const exercise : IExercise | undefined =
                     workout.exerciseList.find(
-                      (element : IExercise) => element.id == data.exerciseId
+                      (element : IExercise) => element.id === data.exerciseId
                     )
-        if (exercise != undefined) {
+        if (exercise !== undefined) {
           exercise.name = data.name
         } else {
           console.warn('exercise not found')
@@ -105,14 +105,14 @@ export const mutations = {
   changeWorkoutTitle (state : State, data : {
         workoutId : string,
         title : string
-    }) {
+    }) : void {
     console.log(state)
-    if (state.workouts != undefined) {
+    if (state.workouts !== undefined) {
       const workout : IWorkout | undefined =
                 state.workouts.find(
-                  (element : IWorkout) => element._id == data.workoutId
+                  (element : IWorkout) => element._id === data.workoutId
                 )
-      if (workout != undefined) {
+      if (workout !== undefined) {
         workout.title = data.title
         console.log(state)
       } else {
@@ -122,37 +122,37 @@ export const mutations = {
       console.warn('state.workouts undefined')
     }
   },
-  deleteExercise (state : State, data : any) {
-    if (state.workouts != undefined) {
+  deleteExercise (state : State, data : any) : void {
+    if (state.workouts !== undefined) {
       const workout : IWorkout | undefined =
-                state.workouts.find(element => element._id == data.workoutId)
-      if (workout != undefined) {
+                state.workouts.find(element => element._id === data.workoutId)
+      if (workout !== undefined) {
         const exercise : IExercise | undefined =
                     workout.exerciseList.find(
-                      (element : IExercise) => element.id == data.exerciseId
+                      (element : IExercise) => element.id === data.exerciseId
                     )
-        if (exercise != undefined) {
+        if (exercise !== undefined) {
           const index : number = workout.exerciseList.indexOf(exercise)
           workout.exerciseList.splice(index, 1)
         }
       }
     }
   },
-  setWorkouts (state : State, workouts : IWorkout[]) {
+  setWorkouts (state : State, workouts : IWorkout[]) : void {
     state.workouts = workouts
   },
-  deleteRepetition (state : State, data : any) {
-    if (state.workouts != undefined) {
+  deleteRepetition (state : State, data : any) : void {
+    if (state.workouts !== undefined) {
       const workout : IWorkout | undefined =
-                state.workouts.find(element => element._id == data.workoutId)
-      if (workout != undefined) {
+                state.workouts.find(element => element._id === data.workoutId)
+      if (workout !== undefined) {
         const exercise : IExercise | undefined =
                     workout.exerciseList.find(
-                      (element : IExercise) => element.id == data.exerciseId
+                      (element : IExercise) => element.id === data.exerciseId
                     )
-        if (exercise != undefined) {
-          const rep = exercise.set.find(rep => rep.id == data.repetitionId)
-          if (rep != undefined) {
+        if (exercise !== undefined) {
+          const rep = exercise.set.find(rep => rep.id === data.repetitionId)
+          if (rep !== undefined) {
             const index : number = exercise.set.indexOf(rep)
             exercise.set.splice(index, 1)
           }
@@ -160,10 +160,10 @@ export const mutations = {
       }
     }
   },
-  setWorkingOut (state : State, bool : boolean) {
+  setWorkingOut (state : State, bool : boolean) : void {
     state.workingOut = bool
   },
-  setCurrentWorkout (state : State, workout : IWorkout) {
+  setCurrentWorkout (state : State, workout : IWorkout) : void {
     state.currentWorkout = workout
   }
 }
