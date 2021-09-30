6628763563792749e726593e7dfdaac791c35722
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-09-30 10:08:14 +0200

    Add function types

--- src/api/exercise.ts
+++ src/api/exercise.ts
@@ -4,14 +4,14 @@ export async function renameExercise (apiInstance : AxiosInstance, data : {
     workoutId: string,
     exerciseId : string,
     name : string
-}) {
+}) : Promise<void> {
   apiInstance.put('/workout/rename_exercise', {
     id: data.workoutId,
     exerciseId: data.exerciseId,
     name: data.name
   }).then((res) => {
-    if (res.status == 200) {
-
+    if (res.status !== 200) {
+      Error('request did not succeed')
     }
   }).catch((err) => {
     console.trace()
@@ -22,10 +22,10 @@ export async function renameExercise (apiInstance : AxiosInstance, data : {
 export async function deleteExercise (apiInstance : AxiosInstance, data : {
     workoutId : string,
     exerciseId : string
-}) {
+}) : Promise<void> {
   apiInstance.put('/workout/delete_exercise', data).then((res) => {
-    if (res.status == 200) {
-
+    if (res.status !== 200) {
+      Error('request did not succeed')
     }
   }
   )
